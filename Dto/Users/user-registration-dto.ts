92d17fd59763a867df715cf7096062c882824fcd
biguard1 <bogdasha05@gmail.com>
2024-08-12 01:07:50 +0300

    Add user registration dto

---
+++ Dto/Users/user-registration-dto.ts
@@ -0,0 +1,26 @@
+/**
+ * This is a TypeGen auto-generated file.
+ * Any changes made to this file can be lost when this file is regenerated.
+ */
+import { z } from 'zod';
+
+export interface UserRegistrationDto {
+    userName: string;
+    email: string;
+    password: string;
+    role: string;
+}
+
+export const userRegistrationDtoProperties: (keyof UserRegistrationDto)[] = [
+    'userName',
+    'email',
+    'password',
+    'role'
+];
+
+export const userRegistrationDtoSchema = z.object({
+    userName: z.string().length(255).regex(/\S/),
+    email: z.string().email().regex(/\S/),
+    password: z.string().regex(/\S/).regex(/^.{6,}$/),
+    role: z.string().regex(/\S/)
+});
