329d577c2edd8e5c960922cb27d355fba9777785
biguard1 <bogdasha05@gmail.com>
2024-08-26 07:17:08 +0300

    Tables improve, and some other updates

--- Dto/WithSeoAddition/Animes/get-light-anime-dto.ts
+++ Dto/WithSeoAddition/Animes/get-light-anime-dto.ts
@@ -6,30 +6,31 @@
 import { getKindDtoSchema } from "../Kinds/get-kind-dto";
 import { getStatusDtoSchema } from "../Statuses/get-status-dto";
 import { getPeriodDtoSchema } from "../Periods/get-period-dto";
-import { getRestrictedRatingDtoSchema } from "../RestrictedRatings/get-restricted-rating-dto";import { getSeoAdditionDtoSchema } from '../../SeoAdditions/get-seo-addition-dto';
-import { z } from 'zod';
+import { getRestrictedRatingDtoSchema } from "../RestrictedRatings/get-restricted-rating-dto";
+import { getSeoAdditionDtoSchema } from "../../SeoAdditions/get-seo-addition-dto";
+import { z } from "zod";
 
 export const getLightAnimeDtoSchema = z.object({
-    kind: getKindDtoSchema,
-    status: getStatusDtoSchema,
-    period: getPeriodDtoSchema,
-    restrictedRating: getRestrictedRatingDtoSchema,
-    name: z.string(),
-    imageName: z.string().nullish(),
-    romajiName: z.string().nullish(),
-    nativeName: z.string(),
-    posterPathUrl: z.string(),
-    posterColors: z.array(z.number().int().min(-2147483648).max(2147483647)),
-    avgDuration: z.number(),
-    tmdbId: z.number().int().min(-9223372036854775808).max(9223372036854775807).nullish(),
-    shikimoriId: z.number().int().min(-9223372036854775808).max(9223372036854775807).nullish(),
-    shikimoriScore: z.number(),
-    tmdbScore: z.number(),
-    imdbScore: z.number(),
-    isPublished: z.boolean(),
-    publishedAt: z.date().nullish(),
-    seoAddition: getSeoAdditionDtoSchema,
-    id: z.string().uuid()
+  kind: getKindDtoSchema,
+  status: getStatusDtoSchema,
+  period: getPeriodDtoSchema,
+  restrictedRating: getRestrictedRatingDtoSchema,
+  name: z.string(),
+  imageName: z.string().nullish(),
+  romajiName: z.string().nullish(),
+  nativeName: z.string(),
+  posterPathUrl: z.string(),
+  posterColors: z.array(z.number().int().min(-2147483648).max(2147483647)),
+  avgDuration: z.number(),
+  tmdbId: z.number().int().min(-9223372036854775808).max(9223372036854775807).nullish(),
+  shikimoriId: z.number().int().min(-9223372036854775808).max(9223372036854775807).nullish(),
+  shikimoriScore: z.number(),
+  tmdbScore: z.number(),
+  imdbScore: z.number(),
+  isPublished: z.boolean(),
+  publishedAt: z.date().nullish(),
+  seoAddition: getSeoAdditionDtoSchema,
+  id: z.string().uuid()
 });
 
 export type GetLightAnimeDto = z.infer<typeof getLightAnimeDtoSchema>;
