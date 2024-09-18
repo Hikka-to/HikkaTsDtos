9b30c18e4fdeda1647af0e79f6b40c7b9f6151ec
biguard1 <bogdasha05@gmail.com>
2024-09-18 08:46:08 +0300

    Filtering update

--- Dto/SharedDtos/filter-dto.ts
+++ Dto/SharedDtos/filter-dto.ts
@@ -2,12 +2,14 @@
  * This is a TypeGen auto-generated file.
  * Any changes made to this file can be lost when this file is regenerated.
  */
-import { z } from 'zod';
+
+import { filterTypeSchema } from "./filter-type";import { z } from 'zod';
 
 export const filterDtoSchema = z.object({
     searchTerm: z.string(),
     column: z.string(),
-    isStrict: z.boolean()
+    filterType: filterTypeSchema,
+    negate: z.boolean()
 });
 
 export type FilterDto = z.infer<typeof filterDtoSchema>;
