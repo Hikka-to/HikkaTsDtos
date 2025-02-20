9cc47ce18282f940d8c4f6692cb73fd30451d68e
Black foxi <dentef767@gmail.com>
2025-02-20 14:46:53 +0000

    add source model

--- Dto/WithSeoAddition/Sources/create-source-dto.ts
+++ Dto/WithSeoAddition/Sources/create-source-dto.ts
@@ -2,12 +2,15 @@
  * This is a TypeGen auto-generated file.
  * Any changes made to this file can be lost when this file is regenerated.
  */
-import { createSeoAdditionDtoSchema } from '../../SeoAdditions/create-seo-addition-dto';
+
+import { createSeoAdditionDtoSchema } from '../../SeoAdditions/create-seo-addition-dto';
 import { z } from 'zod';
 
 export const createSourceDtoSchema = z.object({
-    name: z.string().length(64),
-    seoAddition: createSeoAdditionDtoSchema
+    name: z.string().max(64).default(''),
+    seoAddition: createSeoAdditionDtoSchema.default(
+        createSeoAdditionDtoSchema.parse({})
+    )
 });
 
 export type CreateSourceDto = z.infer<typeof createSourceDtoSchema>;
--- Dto/WithSeoAddition/Sources/update-source-dto.ts
+++ Dto/WithSeoAddition/Sources/update-source-dto.ts
@@ -2,11 +2,12 @@
  * This is a TypeGen auto-generated file.
  * Any changes made to this file can be lost when this file is regenerated.
  */
-import { updateSeoAdditionDtoSchema } from '../../SeoAdditions/update-seo-addition-dto';
+
+import { updateSeoAdditionDtoSchema } from '../../SeoAdditions/update-seo-addition-dto';
 import { z } from 'zod';
 
 export const updateSourceDtoSchema = z.object({
-    name: z.string().length(64),
+    name: z.string().max(64),
     seoAddition: updateSeoAdditionDtoSchema,
     id: z.string().uuid()
 });
